bae9bdfd7d4545b1cca3a4cb6f00706156d5139f
yup <yup@gmail.com>
2021-03-25 17:15:36 -0400

    resolved promise, callback confusion

--- backend/helpers/hashPass.js
+++ backend/helpers/hashPass.js
@@ -3,20 +3,34 @@ import bcrypt from 'bcrypt';
 // this function hashes the password the user input at POST /signup
 const hashPass = (password) => {
     const saltRounds = 10;
-
-    // synchronus
-    const hashPass = bcrypt.hashSync(password, saltRounds);
-    return hashPass;
+    // solution one: using the the hash funciton in a synchronous way
+    // return bcrypt.hashSync(password, saltRounds);
 
     // async callback returns null
-    // bcrypt.hash(password, saltRounds, function(err, result) {
-    //     if (err) {
-    //         console.log('could not hash password');
-    //         throw new Error('could not hash password');
-    //     }
-    //     console.log('hashed password')
-    //     return result;
-    // })
+    bcrypt.hash(password, saltRounds, function(err, result) {
+        if (err) {
+            console.log('could not hash password');
+            throw new Error('could not hash password');
+        }
+        console.log('hashed password')
+        return result;
+    })
+
+    // the second solution. using callback together with promise
+    // we want the result(error or the data) for the promise, so we return a promise
+    /*
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRounds, function (err, result) {
+            if (err) {
+                console.log("could not hash password");
+                reject("could not hash password");
+            } else {
+                resolve(result);
+            }
+            console.log("hashed password");
+        });
+    });
+    */
 
     // this promise returns null
     // bcrypt.hash(password, saltRounds)
@@ -25,6 +39,18 @@ const hashPass = (password) => {
     //     return result
     // })
     // .catch(err => console.log(err))
+
+    // the third solution
+    /*
+     return bcrypt
+        .hash(password, saltRounds)
+        .then((result) => {
+            console.log(result);
+            return result;
+        })
+        .catch((err) => console.log(err));
+    */
+
 }
 
 // this function compares the password with the hashed from the database, POST /login.
@@ -41,4 +67,4 @@ const comparePass = (password, hashedPassword) => {
     // .catch(err => console.log(err))
 }
 
-export { hashPass, comparePass }
+export { hashPass, comparePass };

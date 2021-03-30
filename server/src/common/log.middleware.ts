9f59175125a5bb21dee9f5a9cd6c69c33cc3c709
Kate <test@gmail.com>
2021-03-30 12:43:09 -0400

    update logger middleware

--- server/src/common/log.middleware.ts
+++ server/src/common/log.middleware.ts
@@ -4,7 +4,12 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 export class LogMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     const { method, path } = req;
-    console.log(`${method} ${path}`);
+    const timestamp = new Date().toLocaleString();
+    console.log(`${method} ${path} ${timestamp}`);
+    if (['POST', 'PUT', 'PATCH'].includes(method)) {
+      console.log(`data:\n` + JSON.stringify(req.body));
+    }
+    console.log(`\n`)
     next();
   }
 }

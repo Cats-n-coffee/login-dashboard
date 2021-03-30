8c3ba1a51d6c0cc3af6210384b22bf7477640903
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-03-29 20:02:03 -0400

    added .env variables local

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.withCredentials = true;
 axios.interceptors.response.use(
   (res) => res.data,
   (error) => Promise.reject(error)

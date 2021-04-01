e5477c8e1545371dfb118f2208a8438bab6a17aa
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-03-31 22:12:06 -0400

    modified app.js

--- backend/app.js
+++ backend/app.js
@@ -8,21 +8,24 @@ import routes from "./routes/appRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-const corsConf = {
-	credentials: true,
-	origin: true,
-};
+const corsConf = { credentials: true, origin: true };
 app.use(cors(corsConf));
 app.options("*", cors(corsConf));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);
+// for handling 404 errors
+app.all("*", (req, res, next) => {
+  const method = req.method;
+  const msg = `Can't ${method} ${req.path}`;
+  res.status(404).json({ msg });
+});
 
 app.listen(PORT, () => {
-	console.log("my app listening");
+  console.log("my app listening");
 });
 
 export default app;

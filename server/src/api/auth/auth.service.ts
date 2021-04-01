0d14dc8fa0d03a332cc58204891008375692a875
yup <yup@gmail.com>
2021-04-01 08:28:38 -0400

    add type anotation for returning data

--- server/src/api/auth/auth.service.ts
+++ server/src/api/auth/auth.service.ts
@@ -22,27 +22,24 @@ export class AuthService {
     return this.helperService.signUser(user);
   }
 
-  async logoutUser(user: IAutheUser) {
+  async logoutUser(user: IAutheUser): Promise<void> {
     await this.userService.updateUser(user.id, { token: null });
   }
 
-  async renewToken(cookies: any) {
+  async renewToken(cookies: any): Promise<IAutheUser> {
     const { name } = this.confService.get('app.auth.refresh');
     const token = cookies[name];
     if (!token) {
       throw new UnauthorizedException('Wrong credentials');
-      return;
     }
 
     const user = await this.userService.findUser({ token });
     if (!user) {
       throw new UnauthorizedException('Wrong credentials');
-      return;
     }
 
     if (!this.crytoService.verifyRefreshToken(token)) {
       throw new UnauthorizedException(`Credential is expired.`);
-      return;
     }
 
     return this.helperService.signUser(user);

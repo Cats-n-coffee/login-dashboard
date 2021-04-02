88a76f23a4183b421f1576a064e0c45f79f5d8d1
Kate <test@gmail.com>
2021-04-02 14:39:13 -0400

    add github icon

--- frontend/src/app.unAuthed.js
+++ frontend/src/app.unAuthed.js
@@ -5,6 +5,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import LoginScreen from "screens/login";
 import SignUpScreen from "screens/signup";
 import { Content } from "components/lib";
+import { Github } from "components/Icons";
 
 export default function UnAuthApp() {
   return (
@@ -24,7 +25,24 @@ export default function UnAuthApp() {
         </Switch>
       </Content>
       <footer>
-        <Content>Dashboard application</Content>
+        <Content
+          css={`
+            padding: 1rem 0;
+            text-align: center;
+            svg {
+              max-height: 1.5rem;
+            }
+          `}
+        >
+          <a
+            target="_blank"
+            href="https://github.com/Cats-n-coffee/login-dashboard"
+            rel="noreferrer"
+            title="view source code"
+          >
+            <Github />
+          </a>
+        </Content>
       </footer>
     </>
   );
